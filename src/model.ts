4be4ccde74f361c5c4f5d1048a5bd1d80686fc52
lennartstoelting <l.stoelting@campus.tu-berlin.de>
2024-03-19 18:24:43 +0100

    error handling of initBinaryMatrix function

--- src/model.ts
+++ src/model.ts
@@ -4,7 +4,7 @@ class Model {
     constructor(tilesAcross: number) {
         // grundsätzlich: unspielbar, Summenfeld, spielbar
         // Idee: 13 bit Zahl (bit 0: spielbar ja oder nein? bit 1-12: Summenfeld)
-        this.matrix = this.initBinaryMatrix(test);
+        this.matrix = this.initBinaryMatrix(easy1);
         console.log(this.matrix);
     }
 
@@ -13,29 +13,34 @@ class Model {
         matrix.forEach((row, y) => {
             let binaryRow: number[] = [];
             row.forEach((tile, x) => {
-                if (tile === null) {
+                if (tile === 0) {
                     binaryRow.push(0);
                     return;
                 }
-
                 if (tile === 1) {
-                    binaryRow.push(parseInt("1111111111", 2));
+                    binaryRow.push(parseInt("1".repeat(10), 2));
                     return;
                 }
 
                 // for all other cases, we see them as a decimal number. the 0th bit is 0, then the next 6 bit incript the two numbers to the right of the comma, and the last 6 bit incript the two numbers to the left of the comma
-                // also this needs to check for potential errors in the input matrix
-                let colAndRow = tile.toFixed(2).split(".");
-                let binaryRight = parseInt(colAndRow[1]).toString(2);
-                let binaryDown = parseInt(colAndRow[0]).toString(2);
-                // if the binary number is shorter than 6 bit, we add leading zeros
-                while (binaryRight.length < 6) {
-                    binaryRight = "0" + binaryRight;
+                let colAndRow = tile
+                    .toFixed(2)
+                    .split(".")
+                    .map((sum) => parseInt(sum));
+
+                // error handling
+                if (colAndRow.length !== 2) {
+                    throw new Error("invalid input matrix at x: " + x + " and y: " + y);
                 }
-                while (binaryDown.length < 6) {
-                    binaryDown = "0" + binaryDown;
+                if (colAndRow[0] > 45 || colAndRow[0] == 2 || colAndRow[0] == 1) {
+                    throw new Error("invalid input matrix: sum of col at x: " + x + " and y: " + y + " is given as " + colAndRow[0]);
                 }
-                let binary = parseInt(binaryDown + binaryRight, 2) << 1;
+                if (colAndRow[1] > 45 || colAndRow[1] == 2 || colAndRow[1] == 1) {
+                    throw new Error("invalid input matrix: sum of row at x: " + x + " and y: " + y + " is given as " + colAndRow[1]);
+                }
+
+                let colAndRowBinary = colAndRow.map((sum) => ("000000" + sum.toString(2)).slice(-6));
+                let binary = parseInt(colAndRowBinary[0] + colAndRowBinary[1], 2) << 1;
                 binaryRow.push(binary);
             });
             binaryMatrix.push(binaryRow);
@@ -46,15 +51,15 @@ class Model {
 
 export default Model;
 
-const test: number[][] = [
-    [null, null, 45, 3, null, null, null, 3, 45, null],
-    [null, 17.08, 1, 1, null, 16, 4.03, 1, 1, null],
+const easy1: number[][] = [
+    [0, 0, 45, 3, 0, 0, 0, 3, 45, 0],
+    [0, 17.08, 1, 1, 0, 16, 4.03, 1, 1, 0],
     [0.11, 1, 1, 1, 16.17, 1, 1, 1, 1, 17],
     [0.17, 1, 1, 3.17, 1, 1, 1, 0.16, 1, 1],
-    [null, 0.18, 1, 1, 1, null, null, 17.13, 1, 1],
-    [null, 17.14, 1, 1, null, null, 3.11, 1, 1, null],
-    [0.9, 1, 1, null, 16, 3.16, 1, 1, 1, 4],
+    [0, 0.18, 1, 1, 1, 0, 0, 17.13, 1, 1],
+    [0, 17.04, 1, 1, 0, 0, 3.11, 1, 1, 0],
+    [0.09, 1, 1, 0, 16, 3.16, 1, 1, 1, 4],
     [0.14, 1, 1, 3.1, 1, 1, 1, 16.12, 1, 1],
-    [null, 0.19, 1, 1, 1, 1, 0.18, 1, 1, 1],
-    [null, 0.5, 1, 1, null, null, 0.1, 1, 1, null],
+    [0, 0.19, 1, 1, 1, 1, 0.18, 1, 1, 1],
+    [0, 0.05, 1, 1, 0, 0, 0.1, 1, 1, 0],
 ];
